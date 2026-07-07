8ddaff47c356482082cc102718a7adac43d21c81
ksenev <kalanasenev@gmail.com>
2026-07-07 16:58:29 +1000

    Add files via upload

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "strongshift-v28";
+const CACHE = "strongshift-v29";
 const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", e => {
